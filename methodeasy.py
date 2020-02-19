4249f9496aa409436be3d0d813645bfdfbdebc5a
Jacó Magalhães <41209786+jacomaga@users.noreply.github.com>
2020-02-19 10:21:06 -0300

    Update methodeasy.py

--- methodeasy.py
+++ methodeasy.py
@@ -7,5 +7,5 @@ id_book=input("Insert the book's ID:")
 nm_pages= int(input("Insert the total number of pages:"))
 i=0
 while i <= nm_pages:
-    urllib.request.urlretrieve("http://staticbv.am4.com.br/publicacoes/"+id_book+"/p_"+str(i)+".jpg","p_"+str(i)+".jpg")
+    urllib.request.urlretrieve("http://URL.com.br/publicacoes/"+id_book+"/p_"+str(i)+".jpg","p_"+str(i)+".jpg")
     i += 1
